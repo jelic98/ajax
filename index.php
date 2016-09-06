3978356d78a896addd569bd2e5d4233996717300
jelic98 <jelic.ecloga@gmail.com>
2016-09-06 04:42:35 +0200

    Frontend added

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <html>
 	<head>
 		<title>AJAX Player</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
 	</head>
 	<body onload="update()">
+		<div class="center">
 		<?php
 			include 'connection.php';
 
@@ -10,14 +12,17 @@
 				$cmd = 'SELECT * FROM `rooms`;';
 				$result = mysqli_query($connect, $cmd);
 
+				echo '<h1>Lobby</h1>';
+
 				while($row = mysqli_fetch_array($result)) {
-					echo '<a href="?room='.$row['id'].'">'.$row['name'].'</a>';
+					echo '<a class="item" href="index.php?room='.$row['id'].'">'.$row['name'].'</a>';
 					echo '<br/>';
 				}
 
 				echo '<form action="add_room.php" method="get">';
-				echo '<input type="text" name="name" placeholder="Room name" autofocus required>';
-				echo '<input type="submit" value="Add">';
+				echo '<input type="text" name="name" placeholder="Room name" required>';
+				echo '<br/>';
+				echo '<input type="submit" value="+">';
 				echo '</form>';
 			}else {
 				$room_id = $_GET['room'];
@@ -32,19 +37,23 @@
 				echo '<h1>'.$room_name.'</h1>';
 
 				echo '<form action="upload.php?room='.$room_id.'" method="post" enctype="multipart/form-data">';
-				echo '<input type="file" name="song" autofocus required>';
-				echo '<input type="submit" value="Upload">';
+				echo '<label class="myLabel">';
+			    echo '<input type="file" name="song" required/>';
+			    echo '<span>Upload</span>';
+				echo '</label>';
+				echo '<input type="submit" value="&#8593;">';
 				echo '</form>';
 
-				echo '<p id="status">empty</p>';
-
-				echo '<button type="button" onclick="setStatus()">Play/Pause</button>';
-				echo '<br/>';
-				echo '<a href="index.php">Back</a>';
+				echo '<p id="status">No track</p>';
+				
+				echo '<a class="button" href="index.php">&#8592;</a>';
+				echo '<a id="button" class="button" onclick="setStatus()">&#9658;</a>';
+				echo '<a class="button" href="index.php?room='.$room_id.'"><b>R</b></a>';
 			}
 
 			mysqli_close($connect);
 		?>
+		</div>
 		<script>
 			var audio = new Audio("<?php echo $room_name; ?>/song.mp3");
 
@@ -69,6 +78,8 @@
 				var xhttp = new XMLHttpRequest();
 				xhttp.open("GET", "set_status.php?room=<?php echo $room_id; ?>", true);
 				xhttp.send();
+
+				return false;
 			}
 
 			function getStatus() {
@@ -76,19 +87,30 @@
 
 				xhttp.onreadystatechange = function() {
 					if(this.readyState == 4 && this.status == 200) {
+						var element = document.getElementById("status");
+						var button = document.getElementById("button");			
+
 						switch(this.responseText) {
 							case "play":
 								play();
+								button.innerHTML = "&#10074;&#10074;";
+								element.innerHTML = "Playing";
 								break;
 							case "pause":
 								pause();
+								button.innerHTML = "&#9658;";
+								element.innerHTML = "Paused";
 								break;
 							case "change":
 								change();
+								button.innerHTML = "&#9658;";
+								element.innerHTML = "Ready";
+								break;
+							case "empty":
+								button.innerHTML = "&#9658;";
+								element.innerHTML = "No track";
 								break;
 						}
-
-						document.getElementById("status").innerHTML = this.responseText;
 					}
 				};
 										    
